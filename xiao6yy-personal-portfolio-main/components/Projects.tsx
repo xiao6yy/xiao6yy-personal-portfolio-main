2532f231ea1a44e592247ba84850db563856fca8
xiao6yy <137134746+xiao6yy@users.noreply.github.com>
2024-10-13 21:58:13 +0800

    Update Projects.tsx

--- xiao6yy-personal-portfolio-main/components/Projects.tsx
+++ xiao6yy-personal-portfolio-main/components/Projects.tsx
@@ -31,7 +31,7 @@ export default function Projects() {
       <Link
         className="group tracing-wide font-semibold hover:underline hover:underline-offset-4 hover:decoration-pink text-slate-800 dark:text-slate-400 w-full flex gap-1 items-center justify-center mt-10"
         target="_blank"
-        href="https://github.com/Codefreyy?tab=repositories"
+        href="https://github.com/xiao6yy?tab=repositories"
       >
         View All Projects
         <FaAngleRight className="group-hover:translate-x-2 transition" />
